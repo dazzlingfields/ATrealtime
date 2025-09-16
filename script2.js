cc1f82edf87f87f9fe248df6b88ef0d1f8a06e9d
dazzlingfields <nonstopclash35@gmail.com>
2025-09-17 10:12:09 +1200

    Create script2.js

---
+++ script2.js
@@ -0,0 +1,5 @@
+// --- working ---
+const atApiKey = "18e2ee8ee75d4e6ca7bd446ffa9bd50f";
+// v2.3 - UPDATED URL
+const realtimeUrl = "https://api.at.govt.nz/v2/realtime/vehiclelocations"; 
+const routesUrl = "https://api.at.govt.nz/v3/gtfs/routes";
