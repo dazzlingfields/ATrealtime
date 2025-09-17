e0bfe9491ee95cdb7c0703aaf7f69289410fb736
dazzlingfields <nonstopclash35@gmail.com>
2025-09-17 14:36:09 +1200

    Update realtime.js

--- api/realtime.js
+++ api/realtime.js
@@ -1,13 +1,14 @@
 export default async function handler(req, res) {
   try {
-    const response = await fetch("https://api.at.govt.nz/realtime/vehiclelocations", {
-      headers: {
-        "Ocp-Apim-Subscription-Key": process.env.AT_API_KEY
-      }
+    const response = await fetch("https://api.at.govt.nz/realtime/legacy", {
+      headers: { "Ocp-Apim-Subscription-Key": process.env.AT_API_KEY }
     });
 
-    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(`Upstream error: ${response.status} ${response.statusText}`);
+    }
 
+    const data = await response.json();
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).json(data);
   } catch (err) {
