2f1dbde4c8f4e96a601c204a5ad061f72def780b
dazzlingfields <nonstopclash35@gmail.com>
2025-09-17 14:39:23 +1200

    Create stops.js

---
+++ api/stops.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  try {
+    const { id } = req.query;
+    const url = id
+      ? `https://api.at.govt.nz/gtfs/v3/stops/${id}`
+      : "https://api.at.govt.nz/gtfs/v3/stops";
+
+    const response = await fetch(url, {
+      headers: { "Ocp-Apim-Subscription-Key": process.env.AT_API_KEY }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Upstream error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
